3106153f440c7632d804b0a55be447fb7424385d
RickSantiago <santhiagoxp7@gmail.com>
2017-09-15 17:51:07 -0300

    Adicionando arquivo e importando dependencia do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import {AngularFireModule} from 'angularfire2'
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -18,6 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    //Inicializando angular fire credenciais para dashboard
+    AngularFireModule.initializeApp()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
